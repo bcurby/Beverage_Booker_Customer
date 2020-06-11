11d55a3c981b5fe084e6655e5d9664b43a782beb
jakedurn <jakeadurnford@gmail.com>
2020-06-11 10:43:56 +1000

    UI Branch Made
    Signed-off-by: jakedurn <jakeadurnford@gmail.com>

--- app/src/main/java/com/beveragebooker/customer_app/api/RetrofitClient.java
+++ app/src/main/java/com/beveragebooker/customer_app/api/RetrofitClient.java
@@ -12,7 +12,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://192.168.1.3/BeverageApi/public/";
+    private static final String BASE_URL = "http://192.168.1.176/BeverageApi/public/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
