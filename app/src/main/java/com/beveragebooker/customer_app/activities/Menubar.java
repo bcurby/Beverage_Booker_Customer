b7307257f81896b0fdf3ab1bc4f8b90fde168ea8
Jake Durnford <53285682+jakedurnford@users.noreply.github.com>
2020-07-26 18:51:29 +1000

    Menubar fragment added to cart activity to test it out and it links to the correct places.
    Signed-off-by: jakedurn <jakeadurnford@gmail.com>

--- app/src/main/java/com/beveragebooker/customer_app/activities/Menubar.java
+++ app/src/main/java/com/beveragebooker/customer_app/activities/Menubar.java
@@ -1,26 +1,57 @@
 package com.beveragebooker.customer_app.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import com.beveragebooker.customer_app.R;
+
 
 
 public class Menubar extends Fragment {
 
-    public Menubar() {
-        // Required empty public constructor
+
+    public Menubar(){
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menubar, container, false);
+        View v = inflater.inflate(R.layout.fragment_menubar, container, false);
+        return v;
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        Button homeButton = (Button) view.findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                Intent intent = new Intent(Menubar.super.getActivity(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button accountButton = (Button) view.findViewById(R.id.accountButton);
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                Intent intent = new Intent(Menubar.super.getActivity(), AccountActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button cartButton = (Button) view.findViewById(R.id.cartButton);
+        cartButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                Intent intent = new Intent(Menubar.super.getActivity(), CartActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
