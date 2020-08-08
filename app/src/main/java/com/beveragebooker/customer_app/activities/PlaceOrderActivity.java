5710d8d583d4d7137ea66b457e76da6f6d2a6f41
bcurby <41361395+bcurby@users.noreply.github.com>
2020-08-08 19:50:05 +1000

    026 - Stripe - Adjustment to placeOrder and createNewDelivery to be called upon payment confirmation. Deleted PaymentActivity dummy payment system. Adjusted how variables are passed between activities. Adjusted API call to remove dummy credit card variables.

--- app/src/main/java/com/beveragebooker/customer_app/activities/PlaceOrderActivity.java
+++ app/src/main/java/com/beveragebooker/customer_app/activities/PlaceOrderActivity.java
@@ -29,6 +29,7 @@ import com.stripe.android.Stripe;
 import com.stripe.android.model.ConfirmPaymentIntentParams;
 import com.stripe.android.model.PaymentIntent;
 import com.stripe.android.model.PaymentMethodCreateParams;
+import com.stripe.android.model.SourceTypeModel;
 import com.stripe.android.model.StripeIntent;
 import com.stripe.android.view.CardInputWidget;
 
@@ -60,9 +61,6 @@ import static com.beveragebooker.customer_app.storage.SharedPrefManager.*;
 
 public class PlaceOrderActivity extends AppCompatActivity {
 
-    private int creditCardCVV, expiryMonth, expiryYear;
-    private long creditCardNumber;
-
     private String streetNumber, streetName;
     private int deliveryStatusInt;
     private boolean deliveryStatus;
@@ -73,15 +71,21 @@ public class PlaceOrderActivity extends AppCompatActivity {
     private List<MenuItem> cartItemList;
 
     private TextView orderTotal;
+    private String orderTotalCreditCard;
+    private double doubleOrderTotal;
 
     //Stripe
     private Stripe stripe;
-    //private static final String BACKEND_URL = "https://stripe-payment-beverage.herokuapp.com/"; //after deployed to server
-    private static final String BACKEND_URL = "http://10.0.2.2:5001/";
+
+    //Payment hosting URL on Heroku
+    private static final String BACKEND_URL = "https://stripe-payment-beverage.herokuapp.com/";
+
+    //Local payment server
+    //private static final String BACKEND_URL = "http://10.0.2.2:5001/";
+
     private OkHttpClient httpClient = new OkHttpClient();
     private String paymentIntentClientSecret;
     Button placeOrderButton;
-    CardInputWidget cardInputWidget;
 
     DecimalFormat currency = new DecimalFormat("###0.00");
 
@@ -91,21 +95,25 @@ public class PlaceOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_place_order);
 
         Intent intent = getIntent();
-        creditCardNumber = intent.getLongExtra(PaymentActivity.CREDIT_CARD_NUMBER, 0);
-        creditCardCVV = intent.getIntExtra(PaymentActivity.CREDIT_CARD_CVV, 0);
-        expiryMonth = intent.getIntExtra(PaymentActivity.CREDIT_CARD_EXPIRY_MONTH, 0);
-        expiryYear = intent.getIntExtra(PaymentActivity.CREDIT_CARD_EXPIRY_YEAR, 0);
 
-        streetNumber = intent.getStringExtra(PaymentActivity.STREET_NUMBER);
-        streetName = intent.getStringExtra(PaymentActivity.STREET_NAME);
+        streetNumber = intent.getStringExtra(BookDeliveryActivity.STREET_NUMBER);
+        streetName = intent.getStringExtra(BookDeliveryActivity.STREET_NAME);
 
-        deliveryStatus = intent.getBooleanExtra(PaymentActivity.DELIVERY_STATUS, false);
+        //Assigns delivery status int and order total variable
+        deliveryStatus = intent.getBooleanExtra(BookDeliveryActivity.DELIVERY_STATUS, false);
+        System.out.println(deliveryStatus);
         if(deliveryStatus == true) {
             deliveryStatusInt = 1;
+            orderTotalCreditCard = intent.getStringExtra(BookDeliveryActivity.CART_TOTAL_BOOK_DELIVERY);
+            doubleOrderTotal = Double.parseDouble(orderTotalCreditCard);
+            System.out.println("Credit Card Double Delivery: " + doubleOrderTotal);
         } else {
             deliveryStatusInt = 0;
+            orderTotalCreditCard = intent.getStringExtra(SelectOrderTypeActivity.CART_TOTAL_ORDER_TYPE);
+            doubleOrderTotal = Double.parseDouble(orderTotalCreditCard);
+            System.out.println("Credit Card Double Pick Up: " + doubleOrderTotal);
         }
-        //findViewById(R.id.placeOrderButton).setOnClickListener((View.OnClickListener) this);
+
         placeOrderButton = findViewById(R.id.placeOrderButton);
 
         //Recyclerview
@@ -142,6 +150,7 @@ public class PlaceOrderActivity extends AppCompatActivity {
                 }
                 //Display the total of the items in the order
                 orderTotal.setText("Order Total: $" + currency.format(getOrderTotal()));
+
             }
 
             @Override
@@ -149,6 +158,7 @@ public class PlaceOrderActivity extends AppCompatActivity {
 
             }
         });
+
         //Stripe
         stripe = new Stripe(
                 getApplicationContext(),
@@ -159,14 +169,15 @@ public class PlaceOrderActivity extends AppCompatActivity {
     }
 
     private void startCheckout() {
-        //double total = Double.parseDouble(orderTotal.getText().toString())*100;
-        //System.out.println(orderTotal);
-        double total = 2000;
+
+        //Total is multiplied by 100 as currency is passed to payment gateway in cents
+        int total = (int) Math.rint(doubleOrderTotal * 100);
+        System.out.println(total);
         Map<String, Object> payMap = new HashMap<>();
         Map<String, Object> itemMap = new HashMap<>();
         List<Map<String,Object>> itemList =new ArrayList<>();
-        payMap.put("currency","usd");
-        itemMap.put("id","photo_subscription");
+        payMap.put("currency","aud");
+        itemMap.put("id","cafe_order");
         itemMap.put("amount", total);
         itemList.add(itemMap);
         payMap.put("items", itemList);
@@ -197,9 +208,6 @@ public class PlaceOrderActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-        //});
     }
 
     private void displayAlert(@NonNull String title,
@@ -211,7 +219,8 @@ public class PlaceOrderActivity extends AppCompatActivity {
         builder.setPositiveButton("Ok", null);
         builder.create().show();
     }
-    //Once payment is start, It will call onActivityResult
+
+    //Once payment is started, It will call onActivityResult
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -230,6 +239,7 @@ public class PlaceOrderActivity extends AppCompatActivity {
 
         paymentIntentClientSecret = responseMap.get("clientSecret");
     }
+
     //Ok http call back
     private static final class PayCallBack implements okhttp3.Callback {
         @NonNull private final WeakReference<PlaceOrderActivity> activityWeakReference;
@@ -272,7 +282,7 @@ public class PlaceOrderActivity extends AppCompatActivity {
         }
     }
 
-    private static final class PaymentResultCallBack implements ApiResultCallback<PaymentIntentResult> {
+    private final class PaymentResultCallBack implements ApiResultCallback<PaymentIntentResult> {
         @NonNull private final WeakReference<PlaceOrderActivity> activityWeakReference;
 
         PaymentResultCallBack(@NonNull PlaceOrderActivity activity) {
@@ -300,14 +310,29 @@ public class PlaceOrderActivity extends AppCompatActivity {
             PaymentIntent paymentIntent = paymentIntentResult.getIntent();
             PaymentIntent.Status status = paymentIntent.getStatus();
             if (status == PaymentIntent.Status.Succeeded) {
+
+                System.out.println(deliveryStatus);
                 //Payment completed successfully
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                //Create Delivery in Database and Place Order
+
+                if (deliveryStatus == true) {
+                    System.out.println(deliveryStatus);
+                    createNewDelivery();
+                    placeOrder();
+                } else {
+                    placeOrder();
+                }
+
+                //Displays payment details confirmation alert for testing purposes
+                //Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
                 //If payment is successful there will be details in log and UI
-                Log.i("TAG", "onSuccess:Payment " + gson.toJson(paymentIntent));
-                activity.displayAlert(
-                        "Payment completed",
-                        gson.toJson(paymentIntent)
-                );
+                //Log.i("TAG", "onSuccess:Payment " + gson.toJson(paymentIntent));
+                //activity.displayAlert(
+                        //"Payment completed",
+                       // gson.toJson(paymentIntent)
+                //);
+
             } else if (status == PaymentIntent.Status.RequiresPaymentMethod) {
                 //Payment failed
                 activity.displayAlert(
@@ -318,6 +343,7 @@ public class PlaceOrderActivity extends AppCompatActivity {
         }
     }
 
+    //Places the order in the beverage database after payment is confirmed
     private void placeOrder() {
 
         final User loggedUser = getInstance(PlaceOrderActivity.this).getUser();
@@ -328,13 +354,9 @@ public class PlaceOrderActivity extends AppCompatActivity {
         Call<ResponseBody> call = RetrofitClient
                 .getInstance()
                 .getApi()
-                .placeOrder(userID, creditCardNumber, creditCardCVV, expiryMonth, expiryYear, deliveryStatusInt, orderTotal);
+                .placeOrder(userID, deliveryStatusInt, orderTotal);
 
         System.out.println(userID);
-        System.out.println(creditCardNumber);
-        System.out.println(creditCardCVV);
-        System.out.println(expiryMonth);
-        System.out.println(expiryYear);
         System.out.println(deliveryStatus);
         System.out.println(orderTotal);
 
@@ -363,6 +385,7 @@ public class PlaceOrderActivity extends AppCompatActivity {
         });
     }
 
+    //Creates a new delivery entry in the beverage database after payment is confirmed
     private void createNewDelivery() {
         User loggedUser = getInstance(PlaceOrderActivity.this).getUser();
         int userID = loggedUser.getId();
@@ -389,6 +412,7 @@ public class PlaceOrderActivity extends AppCompatActivity {
         });
     }
 
+    //getter method for returning the order total
     private double getOrderTotal() {
 
         double orderTotal = 0;
@@ -406,11 +430,9 @@ public class PlaceOrderActivity extends AppCompatActivity {
 
             orderTotal += itemTotal;
             System.out.println("Cart Total: " + orderTotal);
-
-
+            
         }
         return orderTotal;
-
     }
 }
 
