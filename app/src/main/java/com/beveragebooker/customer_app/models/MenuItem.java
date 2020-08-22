21fed5dc5206159b913b408024bfb50c474783e7
bcurby <41361395+bcurby@users.noreply.github.com>
2020-08-22 13:33:04 +1000

    034 - View Cart - Updated the CartAdapter to display all selected options for each menu item in the cart. Menu Item object class updated with getter methods. XML for the cart activity and cart item list also updated.

--- app/src/main/java/com/beveragebooker/customer_app/models/MenuItem.java
+++ app/src/main/java/com/beveragebooker/customer_app/models/MenuItem.java
@@ -3,26 +3,26 @@ package com.beveragebooker.customer_app.models;
 public class MenuItem {
 
     private int id;
-
     private String name;
-
     private String description;
-
     private double price;
-
     private int quantity;
-
     private int milk;
-
     private int sugar;
-
     private int decaf;
-
     private int extras;
-
     private int frappe;
-
     private int heated;
+
+    private String itemMilk;
+    private String itemSugar;
+    private String itemDecaf;
+    private String itemVanilla;
+    private String itemCaramel;
+    private String itemChocolate;
+    private String itemWhippedCream;
+    private String itemFrappe;
+    private String itemHeated;
 
 
     public MenuItem(int id, String name, String description, double price) {
@@ -78,12 +78,12 @@ public class MenuItem {
         return milk;
     }
 
-    public int getDecaf() {
-        return decaf;
-    }
-
     public int getSugar() {
         return sugar;
+    }
+
+    public int getDecaf() {
+        return decaf;
     }
 
     public int getExtras() {
@@ -96,6 +96,44 @@ public class MenuItem {
 
     public int getHeated() {
         return heated;
+    }
+
+
+    //Menu item options getters
+    public String getItemMilk() {
+        return itemMilk;
+    }
+
+    public String getItemSugar() {
+        return itemSugar;
+    }
+
+    public String getItemDecaf() {
+        return itemDecaf;
+    }
+
+    public String getItemVanilla() {
+        return itemVanilla;
+    }
+
+    public String getItemCaramel() {
+        return itemCaramel;
+    }
+
+    public String getItemChocolate() {
+        return itemChocolate;
+    }
+
+    public String getItemWhippedCream() {
+        return itemWhippedCream;
+    }
+
+    public String getItemFrappe() {
+        return itemFrappe;
+    }
+
+    public String getItemHeated() {
+        return itemHeated;
     }
 
 
