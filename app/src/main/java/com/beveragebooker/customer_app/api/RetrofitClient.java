b9f228d7b588f063d72e7e1bedad4af8ea1b5977
bcurby <41361395+bcurby@users.noreply.github.com>
2020-09-06 15:29:43 +1000

    058 - Retrofit - Commented out scalars import that sometimes shows as an error

--- app/src/main/java/com/beveragebooker/customer_app/api/RetrofitClient.java
+++ app/src/main/java/com/beveragebooker/customer_app/api/RetrofitClient.java
@@ -14,7 +14,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
+//import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
 
