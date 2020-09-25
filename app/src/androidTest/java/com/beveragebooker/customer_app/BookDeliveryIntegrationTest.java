dd92407648ad46895b3260a98d6dc964deb8c23c
bcurby <41361395+bcurby@users.noreply.github.com>
2020-09-25 20:11:07 +1000

    118 - Fill Cart & Book Delivery - Updated the Fill Cart & Book Delivery Integration Test. Updated the test names to a boolean and set them to run in order. Rewrote the whole test as the delivery address input and payment input have changed since it was written.

--- app/src/androidTest/java/com/beveragebooker/customer_app/BookDeliveryIntegrationTest.java
+++ app/src/androidTest/java/com/beveragebooker/customer_app/BookDeliveryIntegrationTest.java
@@ -1,108 +1,211 @@
 package com.beveragebooker.customer_app;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import com.beveragebooker.customer_app.activities.BrowseMenu;
+import com.beveragebooker.customer_app.activities.PrimaryMenu;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.AllOf;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.AllOf.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class BookDeliveryIntegrationTest {
 
-    @Rule public ActivityScenarioRule<BrowseMenu> ActivityScenarioRule
-            = new ActivityScenarioRule<>(BrowseMenu.class);
+    @Rule public ActivityScenarioRule<PrimaryMenu> ActivityScenarioRule
+            = new ActivityScenarioRule<>(PrimaryMenu.class);
 
     /**
-     * This test runs through the program to test the interaction between
-     * BookDelivery use case and other applicable use cases. Each time the
-     * screen is changed there is a check to make sure that the button that
-     * was the cause of the change is no longer existent confirming that
-     * the screen changed.
+     * Clicks the Drinks Menu button and then adds a 'Long Black' to Cart with:
+     * size = medium
+     * sugar = default
+     * decaf = checked
+     * quantity = 1
+     * comment = thanks
+     **/
+    @Test
+    public void a_IsFillCartForDeliverySuccessful() throws InterruptedException {
+
+        onView(withId(R.id.drinkMenuButton))
+                .perform(click());
+        Thread.sleep(500);
+
+        //Select 'Long Black' from drink menu
+        Thread.sleep(1000);
+        onView(AllOf.allOf(ViewMatchers.withId(R.id.addToCart), hasSibling(withText("Long Black"))))
+                .perform(click());
+
+        //Select medium size drink
+        Thread.sleep(500);
+        onView(withId(R.id.mediumSize))
+                .perform(click());
+        Thread.sleep(500);
+
+        //Select decaf
+        Thread.sleep(500);
+        onView(withId(R.id.decafCheck))
+                .perform(click());
+        Thread.sleep(500);
+
+        //Add comment
+        onView(withId(R.id.editTextComment))
+                .perform(typeText("thanks"), closeSoftKeyboard());
+        Thread.sleep(500);
+
+        //Add to cart
+        onView(withId(R.id.addToCartButton))
+                .perform(click());
+        Thread.sleep(500);
+
+        //Go to cart
+        Thread.sleep(1000);
+        onView(withId(R.id.cartButton))
+                .perform(click());
+        Thread.sleep(500);
+
+        //Check item is in cart
+        Thread.sleep(2000);
+        onView(AllOf.allOf(withId(R.id.cartItemName), hasSibling(withText("Long Black"))))
+                .check(matches(isDisplayed()));
+    }
+
+    /** After adding the item to cart this test then goes through the input process for Checkout.
+     * Delivery address: 1/17 Smith St
+     * Card Number: 4242 4242 4242 4242
+     * Expiry: 12/21
+     * CVC: 123
+     * Postcode: 2345
      */
     @Test
-    public void BookDeliveryIntegrationTest() {
-       /* // BrowseMenu
-        onView(allOf(ViewMatchers.withId(R.id.addToCart), hasSibling(withText("banana"))))
-                .perform(click());
-        onView(withId(R.id.viewCart))
-                .perform(click());
+    public void b_IsBookDeliverySuccessful() throws InterruptedException {
 
-        // CartActivity
-        onView(withId(R.id.viewCart))
-                .check(doesNotExist());
+        onView(withId(R.id.cartButton))
+                .perform(click());
+        Thread.sleep(500);
+
         onView(withId(R.id.checkoutButton))
                 .perform(click());
+        Thread.sleep(500);
 
-        // SelectOrderTypeActivity
-        onView(withId(R.id.checkoutButton))
-                .check(doesNotExist());
         onView(withId(R.id.DeliveryButton))
                 .perform(click());
+        Thread.sleep(500);
 
-        // BookDeliveryActivity
-        onView(withId(R.id.DeliveryButton))
-                .check(doesNotExist());
         onView(withId(R.id.editTextUnit))
-                .perform(typeText("123"));
+                .perform(typeText("1"), closeSoftKeyboard());
+        Thread.sleep(500);
+
         onView(withId(R.id.editTextStreetName))
-                .perform(typeText("StreetName"), closeSoftKeyboard());
+                .perform(typeText("17 Smith St"), closeSoftKeyboard());
+        Thread.sleep(500);
+
         onView(withId(R.id.ProceedToPaymentButton))
                 .perform(click());
+        Thread.sleep(300);
 
-        // PaymentActivity
-        onView(withId(R.id.ProceedToPaymentButton))
-                .check(doesNotExist());
-        onView(withId(R.id.editTextCreditCardNumber1))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber2))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber3))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber4))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCVV))
-                .perform(typeText("123"));
-        onView(withId(R.id.editTextExpiryMonth))
-                .perform(typeText("12"));
-        onView(withId(R.id.editTextExpiryYear))
-                .perform(typeText("21"));
-        onView(withId(R.id.openPlaceOrderButton))
-                .perform(click());
+        Thread.sleep(500);
+        ViewInteraction cardNumberEditText = onView(
+                Matchers.allOf(withId(R.id.card_number_edit_text),
+                        childAtPosition(
+                                childAtPosition(
+                                        Matchers.allOf(withId(R.id.card_number_text_input_layout), withContentDescription("Card number")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        cardNumberEditText.perform(replaceText("4242424242424242") ,closeSoftKeyboard());
 
-        // PlaceOrderActivity
-        onView(withId(R.id.openPlaceOrderButton))
-                .check(doesNotExist());
+        Thread.sleep(500);
+        ViewInteraction expiryDateEditText = onView(
+                Matchers.allOf(withId(R.id.expiry_date_edit_text),
+                        childAtPosition(
+                                childAtPosition(
+                                        Matchers.allOf(withId(R.id.expiry_date_text_input_layout), withContentDescription("Expiration date")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        expiryDateEditText.perform(replaceText("12/21"), closeSoftKeyboard());
+
+        Thread.sleep(500);
+        ViewInteraction cvcEditText = onView(
+                Matchers.allOf(withId(R.id.cvc_edit_text),
+                        childAtPosition(
+                                childAtPosition(
+                                        Matchers.allOf(withId(R.id.cvc_text_input_layout), withContentDescription("CVC")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        cvcEditText.perform(replaceText("123"), closeSoftKeyboard());
+
+        Thread.sleep(500);
+        ViewInteraction postalCodeEditText = onView(
+                Matchers.allOf(withId(R.id.postal_code_edit_text),
+                        childAtPosition(
+                                childAtPosition(
+                                        Matchers.allOf(withId(R.id.postal_code_text_input_layout), withContentDescription("Postal code")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        postalCodeEditText.perform(replaceText("2345"), closeSoftKeyboard());
+
+        //Press Place Order button
+        Thread.sleep(1000);
         onView(withId(R.id.placeOrderButton))
                 .perform(click());
 
-        // OrderConfirmationActivity
-        onView(withId(R.id.placeOrderButton))
-                .check(doesNotExist());
-        onView(withId(R.id.homeButton))
-                .perform(click());
-
-        // PrimaryMenu
-        onView(withId(R.id.menuLink))
+        //Order Confirmation
+        Thread.sleep(5000);
+        onView(withId(R.id.textViewOrderConfirm))
                 .check(matches(isDisplayed()));
-        onView(withId(R.id.editText2))
-                .check(matches(isDisplayed()));*/
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
