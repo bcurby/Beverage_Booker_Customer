bb2f000a4efaa52dfc61cf66dca6139285d3736f
bcurby <41361395+bcurby@users.noreply.github.com>
2020-09-25 19:36:42 +1000

    117 - Fill Cart & Place Order - Updated the Fill Cart & Place Order Integration Test. Updated the test names to a boolean and set them to run in order. Added longer Thread.sleep throughout to allow time for web server responses. Updated some XML names and credit card input that wasn't working.

--- app/src/androidTest/java/com/beveragebooker/customer_app/activities/FillCartPlaceOrderIntegrationTest.java
+++ app/src/androidTest/java/com/beveragebooker/customer_app/activities/FillCartPlaceOrderIntegrationTest.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -15,14 +17,20 @@ import com.beveragebooker.customer_app.R;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.AllOf;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
@@ -33,105 +41,66 @@ import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FillCartPlaceOrderIntegrationTest {
 
     @Rule
-    public ActivityTestRule<PrimaryMenu> mActivityTestRule = new ActivityTestRule<>(PrimaryMenu.class);
+    public ActivityScenarioRule<PrimaryMenu> mActivityTestRule = new ActivityScenarioRule<>(PrimaryMenu.class);
 
     /**
      * Clicks the Drinks Menu button and then adds a 'Long Black' to Cart with:
      * size = medium
-     * sugar = sweetener
+     * sugar = default
      * decaf = checked
-     * quantity = 2
+     * quantity = 1
      * comment = thanks
      **/
 
     @Test
-    public void isItemAddedToCartandCheckoutSuccessful() {
+    public void a_IsFillCartSuccessful() throws InterruptedException {
 
-        /*ViewInteraction appCompatButton3 = onView(
-                allOf(withId(R.id.menuLink), withText("Drink"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatButton3.perform(click());*/
+        onView(withId(R.id.drinkMenuButton))
+                .perform(click());
+        Thread.sleep(500);
 
-        ViewInteraction appCompatButton4 = onView(
-                allOf(withId(R.id.addToCart), withText("Add To Cart"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("androidx.cardview.widget.CardView")),
-                                        0),
-                                4),
-                        isDisplayed()));
-        appCompatButton4.perform(click());
+        //Select 'Long Black' from drink menu
+        Thread.sleep(1000);
+        onView(AllOf.allOf(ViewMatchers.withId(R.id.addToCart), hasSibling(withText("Long Black"))))
+                .perform(click());
 
-        ViewInteraction appCompatRadioButton = onView(
-                allOf(withId(R.id.mediumSize), withText("Medium"),
-                        childAtPosition(
-                                allOf(withId(R.id.sizeRadioGroup),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                1)),
-                                1),
-                        isDisplayed()));
-        appCompatRadioButton.perform(click());
+        //Select medium size drink
+        Thread.sleep(500);
+        onView(withId(R.id.mediumSize))
+                .perform(click());
+        Thread.sleep(500);
 
-        ViewInteraction appCompatRadioButton2 = onView(
-                allOf(withId(R.id.sweetener), withText("Sweetner"),
-                        childAtPosition(
-                                allOf(withId(R.id.sugarRadioGroup),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                1)),
-                                1),
-                        isDisplayed()));
-        appCompatRadioButton2.perform(click());
+        //Select decaf
+        Thread.sleep(500);
+        onView(withId(R.id.decafCheck))
+                .perform(click());
+        Thread.sleep(500);
 
-        /*ViewInteraction appCompatCheckBox = onView(
-                allOf(withId(R.id.decafCheck), withText("Decaf"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.linearLayout),
-                                        2),
-                                1),
-                        isDisplayed()));
-        appCompatCheckBox.perform(click());*/
+        //Add comment
+        onView(withId(R.id.editTextComment))
+                .perform(typeText("thanks"), closeSoftKeyboard());
+        Thread.sleep(500);
 
-        ViewInteraction appCompatButton5 = onView(
-                allOf(withId(R.id.add_btn), withText("+"),
-                        childAtPosition(
-                                allOf(withId(R.id.layout),
-                                        childAtPosition(
-                                                withId(R.id.qtyButton),
-                                                0)),
-                                2),
-                        isDisplayed()));
-        appCompatButton5.perform(click());
+        //Add to cart
+        onView(withId(R.id.addToCartButton))
+                .perform(click());
+        Thread.sleep(500);
 
-        ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.editTextComment),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        appCompatEditText3.perform(replaceText("thanks"), closeSoftKeyboard());
+        //Go to cart
+        Thread.sleep(1000);
+        onView(withId(R.id.cartButton))
+                .perform(click());
+        Thread.sleep(500);
 
-        ViewInteraction appCompatButton6 = onView(
-                allOf(withId(R.id.addToCartButton), withText("Add to Cart"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                3),
-                        isDisplayed()));
-        appCompatButton6.perform(click());
+        //Check item is in cart
+        Thread.sleep(2000);
+        onView(AllOf.allOf(withId(R.id.cartItemName), hasSibling(withText("Long Black"))))
+                .check(matches(isDisplayed()));
+    }
 
     /** After adding the item to cart this test then goes through the input process for Checkout.
      * Card Number: 4242 4242 4242 4242
@@ -139,38 +108,28 @@ public class FillCartPlaceOrderIntegrationTest {
      * CVC: 123
      * Postcode: 2345
      */
+    @Test
+    public void b_IsPlaceOrderSuccessful() throws InterruptedException {
 
-        ViewInteraction appCompatButton7 = onView(
-                allOf(withId(R.id.cartButton), withText("Cart"),
-                        childAtPosition(
-                                allOf(withId(R.id.menuBarInsert),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.RelativeLayout")),
-                                                0)),
-                                3),
-                        isDisplayed()));
-        appCompatButton7.perform(click());
+        //Go to cart
+        Thread.sleep(1000);
+        onView(withId(R.id.cartButton))
+                .perform(click());
+        Thread.sleep(500);
 
-        ViewInteraction appCompatButton8 = onView(
-                allOf(withId(R.id.checkoutButton), withText("Checkout"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                3),
-                        isDisplayed()));
-        appCompatButton8.perform(click());
+        //Go to checkout
+        Thread.sleep(1000);
+        onView(withId(R.id.checkoutButton))
+                .perform(click());
+        Thread.sleep(500);
 
-        ViewInteraction appCompatButton9 = onView(
-                allOf(withId(R.id.PickUpButton), withText("Pick Up"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatButton9.perform(click());
+        //Select Pick Up
+        Thread.sleep(1000);
+        onView(withId(R.id.PickUpButton))
+                .perform(click());
+        Thread.sleep(500);
 
+        Thread.sleep(500);
         ViewInteraction cardNumberEditText = onView(
                 allOf(withId(R.id.card_number_edit_text),
                         childAtPosition(
@@ -179,8 +138,9 @@ public class FillCartPlaceOrderIntegrationTest {
                                         0),
                                 0),
                         isDisplayed()));
-        cardNumberEditText.perform(replaceText("4242 4242 4242 4242"), closeSoftKeyboard());
+        cardNumberEditText.perform(replaceText("4242424242424242") ,closeSoftKeyboard());
 
+        Thread.sleep(500);
         ViewInteraction expiryDateEditText = onView(
                 allOf(withId(R.id.expiry_date_edit_text),
                         childAtPosition(
@@ -191,6 +151,7 @@ public class FillCartPlaceOrderIntegrationTest {
                         isDisplayed()));
         expiryDateEditText.perform(replaceText("12/21"), closeSoftKeyboard());
 
+        Thread.sleep(500);
         ViewInteraction cvcEditText = onView(
                 allOf(withId(R.id.cvc_edit_text),
                         childAtPosition(
@@ -201,6 +162,7 @@ public class FillCartPlaceOrderIntegrationTest {
                         isDisplayed()));
         cvcEditText.perform(replaceText("123"), closeSoftKeyboard());
 
+        Thread.sleep(500);
         ViewInteraction postalCodeEditText = onView(
                 allOf(withId(R.id.postal_code_edit_text),
                         childAtPosition(
@@ -210,6 +172,17 @@ public class FillCartPlaceOrderIntegrationTest {
                                 0),
                         isDisplayed()));
         postalCodeEditText.perform(replaceText("2345"), closeSoftKeyboard());
+
+        //Press Place Order button
+        Thread.sleep(1000);
+        onView(withId(R.id.placeOrderButton))
+                .perform(click());
+
+        //Order Confirmation
+        Thread.sleep(5000);
+        onView(withId(R.id.textViewOrderConfirm))
+                .check(matches(isDisplayed()));
+        
     }
 
     private static Matcher<View> childAtPosition(
