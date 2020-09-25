050cc9df0cf6cf4f8f51ba8d0e82a9ea8dc831e9
bcurby <41361395+bcurby@users.noreply.github.com>
2020-09-25 16:28:58 +1000

    112 - Place Order - Updated the Place Order Instrumented Test. Added Thread.sleep so the test will pause and give the web server time to respond. Also updated some XML names that had been changed since the test was written. Changed some of the test methods so they were checking for more fields displayed.

--- app/src/androidTest/java/com/beveragebooker/customer_app/InstrumentedTestForPlaceOrder.java
+++ app/src/androidTest/java/com/beveragebooker/customer_app/InstrumentedTestForPlaceOrder.java
@@ -1,228 +1,116 @@
 package com.beveragebooker.customer_app;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import com.beveragebooker.customer_app.activities.CartActivity;
+import com.beveragebooker.customer_app.activities.PrimaryMenu;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.AllOf;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class InstrumentedTestForPlaceOrder {
 
     @Rule
-    public ActivityScenarioRule<CartActivity> ActivityScenarioRule
-            = new ActivityScenarioRule<>(CartActivity.class);
+    public ActivityScenarioRule<PrimaryMenu> ActivityScenarioRule
+            = new ActivityScenarioRule<>(PrimaryMenu.class);
 
     /**
      * Verifies that when checkoutButton is clicked everything on the next page is displayed showing that
      * the application works as intended.
      */
 
-    /*@Test
-    public void GoToCheckOut() {
+    @Test
+    public void a_IsCheckoutButtonsDisplayed() throws InterruptedException {
+
+        Thread.sleep(500);
+        onView(withId(R.id.cartButton))
+                .perform(click());
+
+        //Check the Checkout button is displayed
+        Thread.sleep(1000);
+        onView(withId(R.id.checkoutButton))
+                .check(matches(isDisplayed()));
+
+        Thread.sleep(500);
         onView(withId(R.id.checkoutButton))
                 .perform(click());
+
+        //Check the Pick Up button is displayed
+        Thread.sleep(1000);
+        onView(withId(R.id.PickUpButton))
+                .check(matches(isDisplayed()));
+
+        Thread.sleep(500);
         onView(withId(R.id.PickUpButton))
                 .perform(click());
-        onView(withId(R.id.creditCardNumberLayout))
+
+        //Check the card input widget is displayed
+        Thread.sleep(1000);
+        onView(withId(R.id.cardInputWidget))
                 .check(matches(isDisplayed()));
-        onView(withId(R.id.editTextCreditCardNumber1))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.editTextCreditCardNumber2))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.editTextCreditCardNumber3))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.editTextCreditCardNumber4))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.creditCardCVVLayout))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.editTextCVV))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.creditCardExpiryLayout))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.editTextExpiryMonth))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.editTextExpiryYear))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.openPlaceOrderButton))
-                .check(matches(isDisplayed()));
+
     }
 
     /**
-     * Verifies that when openPlaceOrderButton is clicked and there is an error at
-     * card number where there is a number missing it displays an error instead of continuing.
+     * This is checks that the fields and buttons for payment are displayed correctly.
      */
-    /*@Test
-    public void PaymentInformation_ErrorAtCardNumber_TooLittleNumbers() {
-        onView(withId(R.id.checkoutButton))
-                .perform(click());
-        onView(withId(R.id.PickUpButton))
-                .perform(click());
-        onView(withId(R.id.editTextCreditCardNumber1))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber2))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber3))
-                .perform(typeText("123")); // this is where the error is
-        onView(withId(R.id.editTextCreditCardNumber4))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCVV))
-                .perform(typeText("123"));
-        onView(withId(R.id.editTextExpiryMonth))
-                .perform(typeText("12"));
-        onView(withId(R.id.editTextExpiryYear))
-                .perform(typeText("21"));
-        onView(withId(R.id.openPlaceOrderButton))
-                .perform(click());
-        onView(withId(R.id.editTextCreditCardNumber1))
-                .check(matches(hasErrorText("Credit card number must be 16 digits")));
-    }
+    @Test
+    public void b_IsPaymentFieldsDisplayed() throws InterruptedException {
 
-    /**
-     * Verifies that when openPlaceOrderButton is clicked and there is an error at
-     * CVV where there is a number missing it displays an error instead of continuing.
-     */
-    /*@Test
-    public void PaymentInformation_ErrorAtCVV_TooLittleNumbers() {
-        onView(withId(R.id.checkoutButton))
+        Thread.sleep(500);
+        onView(withId(R.id.cartButton))
                 .perform(click());
-        onView(withId(R.id.PickUpButton))
-                .perform(click());
-        onView(withId(R.id.editTextCreditCardNumber1))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber2))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber3))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber4))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCVV))
-                .perform(typeText("12")); // This is where the error is
-        onView(withId(R.id.editTextExpiryMonth))
-                .perform(typeText("12"));
-        onView(withId(R.id.editTextExpiryYear))
-                .perform(typeText("21"));
-        onView(withId(R.id.openPlaceOrderButton))
-                .perform(click());
-        onView(withId(R.id.editTextCVV))
-                .check(matches(hasErrorText("CVV number must be 3 digits")));
-    }
 
-    /**
-     * Verifies that when openPlaceOrderButton is clicked and there is an error at
-     * Month where an incorrect month is entered it errors instead of continuing.
-     */
-    /*@Test
-    public void PaymentInformation_ErrorAtMonth_OutOfBounds() {
+        Thread.sleep(800);
         onView(withId(R.id.checkoutButton))
                 .perform(click());
-        onView(withId(R.id.PickUpButton))
-                .perform(click());
-        onView(withId(R.id.editTextCreditCardNumber1))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber2))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber3))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber4))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCVV))
-                .perform(typeText("123"));
-        onView(withId(R.id.editTextExpiryMonth))
-                .perform(typeText("30")); // This is where the error is
-        onView(withId(R.id.editTextExpiryYear))
-                .perform(typeText("21"));
-        onView(withId(R.id.openPlaceOrderButton))
-                .perform(click());
-        onView(withId(R.id.editTextExpiryMonth))
-                .check(matches(hasErrorText("Expiry month must be a number between 01 and 12")));
-    }
 
-    /**
-     * Verifies that when openPlaceOrderButton is clicked and there is an error at
-     * year where an out of date year is provided it errors instead of continuing.
-     */
-    /*public void PaymentInformation_ErrorAtYear_OutOfBounds() {
-        onView(withId(R.id.checkoutButton))
-                .perform(click());
+        Thread.sleep(800);
         onView(withId(R.id.PickUpButton))
                 .perform(click());
-        onView(withId(R.id.editTextCreditCardNumber1))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber2))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber3))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber4))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCVV))
-                .perform(typeText("123"));
-        onView(withId(R.id.editTextExpiryMonth))
-                .perform(typeText("12"));
-        onView(withId(R.id.editTextExpiryYear))
-                .perform(typeText("19")); // This is where the error is
-        onView(withId(R.id.openPlaceOrderButton))
-                .perform(click());
-        onView(withId(R.id.editTextExpiryYear))
-                .check(matches(hasErrorText("Expiry year must be a number equal to current year or greater")));
-    }
 
-    /**
-     * This is pretty much simulating an entire order apart from adding the item to cart.
-     * The happy day scenario.
-     */
-    /*@Test
-    public void zPaymentInformation_PlaceOrder_OrderConfirmation_MainMenu() {
-        onView(withId(R.id.checkoutButton))
-                .perform(click());
-        onView(withId(R.id.PickUpButton))
-                .perform(click());
-        onView(withId(R.id.editTextCreditCardNumber1))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber2))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber3))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCreditCardNumber4))
-                .perform(typeText("1234"));
-        onView(withId(R.id.editTextCVV))
-                .perform(typeText("123"));
-        onView(withId(R.id.editTextExpiryMonth))
-                .perform(typeText("12"));
-        onView(withId(R.id.editTextExpiryYear))
-                .perform(typeText("21"));
-        onView(withId(R.id.openPlaceOrderButton))
-                .perform(click());
-        onView(withId(R.id.placeOrderRecyclerView))
-                .check(matches(isDisplayed()));
+        Thread.sleep(800);
         onView(withId(R.id.orderTotal))
                 .check(matches(isDisplayed()));
+
         onView(withId(R.id.placeOrderButton))
                 .check(matches(isDisplayed()));
-        onView(withId(R.id.placeOrderButton))
-                .perform(click());
-        onView(withId(R.id.textViewOrderConfirm))
+
+        //Check the Pick Up button is displayed
+        Thread.sleep(1000);
+        onView(withId(R.id.cardInputWidget))
                 .check(matches(isDisplayed()));
-        onView(withId(R.id.homeButton))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.homeButton))
-                .perform(click());
-        onView(withId(R.id.menuLink))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.editText2))
-                .check(matches(isDisplayed()));
-    }*/
+    }
 }
